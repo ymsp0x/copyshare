e53a352c51b32c3762fa014ef0883b37eec7fe0d
L4ns <Maulanaysp360@gmail.com>
2025-05-30 05:49:15 +0700

    backend 3

---
+++ dist/assets/browser-BdSXyG2l.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-iGQR-3dk.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
